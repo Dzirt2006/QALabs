6950eb057f3d4aab596fd2102de3e52faa7c36d8
Dzirt2006 <dzirt-2006@mail.ru>
2020-10-29 07:46:03 -0400

    fix using DataProvider

--- src/test/java/Lab1TestNg.java
+++ src/test/java/Lab1TestNg.java
@@ -1,23 +1,24 @@
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 
 import java.util.*;
 
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 
 public class Lab1TestNg {
 	Lab1Main inst = new Lab1Main();
-	ArrayList<String> assets;
+	static ArrayList<String> assets;
 
-	
 	/**
-	 * Method for reading input from the console
-	 * reads only 2 times
+	 * Method for reading input from the console reads only 2 times
+	 * 
 	 * @return ArrayList<String>
 	 */
 	private ArrayList<String> input() {
-		ArrayList<String> assets = new ArrayList<String>();
+		assets = new ArrayList<String>();
 		Scanner scan = new Scanner(System.in);
 		System.out.println("type your data and press enter at ther end");
 		String line = scan.nextLine();
@@ -33,27 +34,30 @@ public class Lab1TestNg {
 		return assets;
 	}
 
-	
+	@DataProvider
+	public Object[][] sendData() {
+		return new Object[][] { { assets.get(0) }, { assets.get(1) } };
+	}
+
 	/**
-	 * Test will pass or fail depends on input data
-	 * if input String length less than 3(str.length<3) test will fail 
-	 * because mutator(setStr) in class Lab1Main will throws an error and won't initialize the variable 
+	 * Test will pass or fail depends on input data if input String length less than
+	 * 3(str.length<3) test will fail because mutator(setStr) in class Lab1Main will
+	 * throws an error and won't initialize the variable
 	 */
-	@Test
-	public void firstTest() {
+	@Test(dataProvider="sendData")
+	public void firstTest(String data) {
 		try {
-			inst.setStr(assets.get(0));
+			inst.setStr(data);
 		} catch (StringIndexOutOfBoundsException e) {
 			System.out.println(e);
 		} finally {
-			Assert.assertEquals(inst.getStr(), assets.get(0));
-			assets.remove(0);
+			Assert.assertEquals(inst.getStr(), data);
+//			assets.remove(0);
 		}
 	}
 
-	
 	/**
-	 * does the testNg run test parallel? 
+	 * does the testNg run test parallel?
 	 */
 //	@Test
 //	public void secondtTest() {
@@ -63,17 +67,17 @@ public class Lab1TestNg {
 //			System.out.println(e);
 //		} finally {
 //			Assert.assertEquals(inst.getStr(), assets.get(0));
-//			assets.remove(0);
 //		}
 //	}
 
-	@BeforeTest
+	@BeforeClass
 	public void beforeTest() {
 		assets = input();
 	}
 
 	@AfterTest
 	public void afterTest() {
+
 	}
 
 }
