9d2d41090c9d2414bc31f70793bfd79fd1cf84cc
Dzirt2006 <dzirt-2006@mail.ru>
2020-10-29 09:15:21 -0400

    DataProvider vs dependsOnMethod

--- src/test/java/Lab1TestNg.java
+++ src/test/java/Lab1TestNg.java
@@ -34,41 +34,67 @@ public class Lab1TestNg {
 		return assets;
 	}
 
+	/**
+	 * Tests will pass or fail depends on input data if input String length less
+	 * than 3(str.length<3) test will fail because mutator(setStr) in class Lab1Main
+	 * will throws an error and won't initialize the variable
+	 * 
+	 * In examples below 2 approach
+	 * - DataProvider
+	 * 		we set up a data in object from the ArrayList without any impact on ArrayList
+	 * 		and our test will be run through the all data in the DataProvider
+	 * 
+	 * - dependsOnMethod
+	 * 		in this case we are creating 2 tests and second test will be waiting when first will be executed 
+	 */
+
+	/**
+	 * testS using DataProvider
+	 * @return
+	 */
+
 	@DataProvider
 	public Object[][] sendData() {
 		return new Object[][] { { assets.get(0) }, { assets.get(1) } };
 	}
 
-	/**
-	 * Test will pass or fail depends on input data if input String length less than
-	 * 3(str.length<3) test will fail because mutator(setStr) in class Lab1Main will
-	 * throws an error and won't initialize the variable
-	 */
-	@Test(dataProvider="sendData")
-	public void firstTest(String data) {
+	@Test(dataProvider = "sendData")
+	public void withProvider(String data) {
 		try {
 			inst.setStr(data);
 		} catch (StringIndexOutOfBoundsException e) {
 			System.out.println(e);
 		} finally {
 			Assert.assertEquals(inst.getStr(), data);
-//			assets.remove(0);
 		}
 	}
 
 	/**
-	 * does the testNg run test parallel?
+	 * Same tests using dependsOnMethod
+	 * if first test fail, second test(dependsOnMethods first) will be skipped
 	 */
-//	@Test
-//	public void secondtTest() {
-//		try {
-//			inst.setStr(assets.get(0));
-//		} catch (StringIndexOutOfBoundsException e) {
-//			System.out.println(e);
-//		} finally {
-//			Assert.assertEquals(inst.getStr(), assets.get(0));
-//		}
-//	}
+	@Test
+	public void firstTest() {
+		try {
+			inst.setStr(assets.get(0));
+		} catch (StringIndexOutOfBoundsException e) {
+			System.out.println(e);
+		} finally {
+			Assert.assertEquals(inst.getStr(), assets.get(0));
+			assets.remove(0); // remove first element from List
+		}
+	}
+
+	@Test(dependsOnMethods = "firstTest")
+	public void secondtTest() {
+		try {
+			inst.setStr(assets.get(0));
+		} catch (StringIndexOutOfBoundsException e) {
+			System.out.println(e);
+		} finally {
+			Assert.assertEquals(inst.getStr(), assets.get(0));
+		}
+	}
 
 	@BeforeClass
 	public void beforeTest() {
