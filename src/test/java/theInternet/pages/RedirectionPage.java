cd79b62b7378fd1edc24efba64b4f4709a862cec
Dzirt2006 <dzirt-2006@mail.ru>
2020-11-10 16:06:44 -0500

    notificationMessage

---
+++ src/test/java/theInternet/pages/RedirectionPage.java
@@ -0,0 +1,52 @@
+package theInternet.pages;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+import frameworks.BasePage;
+import frameworks.ElementSearchers;
+
+public class RedirectionPage extends BasePage implements ElementSearchers {
+	String direction = "redirector";
+	String redirectTriggerId = "redirect";
+	List<WebElement> codes;
+	String mainPageUrl;
+
+	public RedirectionPage(String url, WebDriver driver) {
+		super(url, driver);
+	}
+
+	public RedirectionPage navigateTo() {
+		super.navigateTo(direction);
+		return this;
+	}
+
+	public RedirectionPage redirect() {
+		mainPageUrl = super.getUrl();
+		elementById(redirectTriggerId, driver).click();
+		return this;
+	}
+
+	public boolean isRedirected() {
+		System.out.println(mainPageUrl + "\n" + super.getUrl());
+		return mainPageUrl.equals(super.getUrl());
+	}
+
+	//in case if we want to check all redirects
+	public RedirectionPage getListofStatusCodes() {
+		codes=elementsByTag("li", driver);
+		return this;
+	}
+	
+	public RedirectionPage redirectByCode(String code) {
+		Iterator<WebElement> iter=codes.iterator();
+		while(iter.hasNext()) {
+		}
+		return this;
+	}
+
+}
