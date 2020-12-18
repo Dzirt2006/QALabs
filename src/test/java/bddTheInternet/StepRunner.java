546bc1cdd8bd58f773eebb30f10a9294ee111358
Dzirt2006 <Dzirt-2006@mail.ru>
2020-12-18 09:31:52 -0500

    Runner dbb comented

--- src/test/java/bddTheInternet/StepRunner.java
+++ src/test/java/bddTheInternet/StepRunner.java
@@ -6,6 +6,13 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+
+/*
+ * Run this class as JUnit
+ */
+
+
+
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/bddTheInternet/scenario", // the path of the feature files
 		glue = { "stepDefenitions" } // the path of the step definition files
